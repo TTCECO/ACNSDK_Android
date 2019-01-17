47640a245818836e1c966ec389050d60438aaf5b
wuqiong <wuqiong@tataufo.com>
2019-01-17 19:26:09 +0800

    delete

--- behavior/src/main/java/com/ttc/behavior/ui/TTCBannerSize.java
+++
@@ -1,9 +0,0 @@
-package com.ttc.behavior.ui;
-
-/**
- * Created by lwq on 2019/1/10.
- */
-public class TTCBannerSize {
-    public static int BANNER = 1;
-    public static int SMART_BANNER = 2;
-}
